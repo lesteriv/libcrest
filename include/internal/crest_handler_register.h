f32ffa24c79a0ec377ab99033b8b78e188f6387b
lesteriv <igor_nikitin@valentina-db.com>
2012-10-02 15:49:35 +0300

    cosmetic

--- include/internal/crest_handler_register.h
+++ include/internal/crest_handler_register.h
@@ -20,9 +20,9 @@ typedef	void(*crest_api_callback_t)( crest_connection& );
 /**********************************************************************************************/
 // Helper class to auto register handlers by macroese.
 //
-struct crest_handler_register
+struct crest_auto_handler_register
 {
-	crest_handler_register(
+	crest_auto_handler_register(
 		crest_http_method	 method,
 		const char*			 resource,
 		crest_api_callback_t func,
@@ -37,5 +37,5 @@ struct crest_handler_register
 /**********************************************************************************************/
 #define CREST_CPP_HANDLER( method, admin, publ, name ) \
 	static void JOIN( f, __LINE__ )( crest_connection& ); \
-	static crest_handler_register JOIN( r, __LINE__ )( method, name, JOIN( f, __LINE__ ), admin, publ ); \
+	static crest_auto_handler_register JOIN( r, __LINE__ )( method, name, JOIN( f, __LINE__ ), admin, publ ); \
 	static void JOIN( f, __LINE__ )
