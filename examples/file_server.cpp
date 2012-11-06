7be8e8ac77247d98b94510a481e778d8b15797db
lesteriv <igor_nikitin@valentina-db.com>
2012-11-06 14:14:47 +0200

    update examples

--- examples/file_server.cpp
+++ examples/file_server.cpp
@@ -1,32 +1,37 @@
 /**********************************************************************************************/
-/* file_server.cpp								                                			  */
+/* file_server_stl.cpp							                                			  */
 /*                                                                       					  */
 /* Igor Nikitin, 2012																		  */
 /* MIT license			                                                  					  */
 /**********************************************************************************************/
 
 // STD
-#include <stdio.h>
-#include <string.h>
+#include <iostream>
+#include <string>
 
 // CREST
 #include "../include/crest.h"
 
 /**********************************************************************************************/
-static const char	g_root[]	= "/var/log";
+using namespace std;
+
+
+//////////////////////////////////////////////////////////////////////////
+// constants
+//////////////////////////////////////////////////////////////////////////
+
 
 /**********************************************************************************************/
-static const size_t	ROOT_LEN	= sizeof( g_root ) - 1;
-static const size_t	URL_LEN		= 8192;
-static const size_t	BUF_LEN		= URL_LEN + ROOT_LEN + 1;
+static const string	g_root = "/var/log";
 
 /**********************************************************************************************/
-static const char g_index[]	=
+static const string g_index	=
 	"<html>"
 	"<a href=\"apt/history.log\">apt/history.log</a><br>"
 	"<a href=\"Xorg.0.log\">Xorg.0.log</a><br>"
 	"<a href=\"auth.log\">auth.log</a><br>"
 	"<a href=\"dmesg\">dmesg</a><br>"
+	"<a href=\"kern.log\">kern.log</a><br>"
 	"<a href=\"syslog\">syslog</a>"
 	"</html>";
 
@@ -39,19 +44,9 @@ static const char g_index[]	=
 /**********************************************************************************************/
 GET( * )( cr_connection& conn )
 {
-	if( conn.url()[ 1 ] )
-	{
-		char path[ BUF_LEN ];
-		memcpy( path, g_root, ROOT_LEN );
-		strncpy( path + ROOT_LEN, conn.url(), URL_LEN );
-		path[ BUF_LEN - 1 ] = 0;
-		
-		conn.send_file( path, "text/plain" );
-	}
-	else
-	{
-		conn.respond( CR_HTTP_OK, g_index, sizeof( g_index ) - 1 );
-	}
+	conn.url()[ 1 ] ?
+		conn.send_file( g_root + conn.url(), "text/plain" ) :
+		conn.respond( CR_HTTP_OK, g_index );
 }
 
 
@@ -65,5 +60,5 @@ int main( void )
 {
 	cr_options opts;
 	if( !cr_start( opts ) )
-		printf( "%s\n", cr_error_string() );
+		cout << cr_error_string() << endl;
 }
